aa8870598d4a812d2f718dc72f3a13bf326da10c
Manshiro Ogikubo <manshiro@gmail.com>
2011-03-26 21:40:40 +0900

    Test commit.

--- Classes/FirstViewController.m
+++ Classes/FirstViewController.m
@@ -7,6 +7,7 @@
 //
 
 #define S_HOSEI					0.95
+#define HEXCOLOR(c) [UIColor colorWithRed:((c>>16)&0xFF)/255.0 green:((c>>8)&0xFF)/255.0 blue:(c&0xFF)/255.0 alpha:1.0];
 
 #import <QuartzCore/QuartzCore.h>
 #import "FirstViewController.h"
@@ -18,21 +19,21 @@
 @synthesize scrollView;
 
 /*
-// The designated initializer. Override to perform setup that is required before the view is loaded.
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
-	self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-	if (self) {
-        // Custom initialization
-    }
-    return self;
-}
-*/
+ // The designated initializer. Override to perform setup that is required before the view is loaded.
+ - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+ self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+ if (self) {
+ // Custom initialization
+ }
+ return self;
+ }
+ */
 
 /*
-// Implement loadView to create a view hierarchy programmatically, without using a nib.
-- (void)loadView {
-}
-*/
+ // Implement loadView to create a view hierarchy programmatically, without using a nib.
+ - (void)loadView {
+ }
+ */
 
 
 // Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
@@ -43,18 +44,20 @@
     self.dataArray = [iPLDS getAllAlbumJacketData];
     self.scrollView.contentSize = CGSizeMake(320, 480);
 	// set scroll view's background color. 
-	self.scrollView.backgroundColor = [UIColor colorWithRed:(10/255) green:(74/255) blue:(12/255) alpha:1.0];
-
+    
+	self.scrollView.backgroundColor = HEXCOLOR(0x2a4820);
+    //self.scrollView.backgroundColor = [UIColor whiteColor];
+    
     [self setAlbumDataToScrollView];
 }
 
 /*
-// Override to allow orientations other than the default portrait orientation.
-- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
-    // Return YES for supported orientations
-    return (interfaceOrientation == UIInterfaceOrientationPortrait);
-}
-*/
+ // Override to allow orientations other than the default portrait orientation.
+ - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
+ // Return YES for supported orientations
+ return (interfaceOrientation == UIInterfaceOrientationPortrait);
+ }
+ */
 
 - (void)didReceiveMemoryWarning {
 	// Releases the view if it doesn't have a superview.
@@ -73,37 +76,55 @@
     [super dealloc];
 }
 
-#pragma mark -
-#pragma mark scrollViewContents
--(void) setAlbumDataToScrollView {
+static BOOL _albumThreadRunning;
+static NSMutableArray *_albumQueue;
+
+- (void)loadAlbumInBackground {
+    [CATransaction lock];
+    if (!_albumThreadRunning) {
+        [NSThread detachNewThreadSelector:@selector(albumThread:) toTarget:self withObject:nil];
+        _albumThreadRunning = YES;
+    }
+    [CATransaction unlock];
+}
+
+- (void)albumThread:(id)unused {
+    [CATransaction lock];
     int x = 0;
     int y = 0;
-	// 補正後の画像サイズ
-	float _jacket_image_ssize = JACKET_SIZE * S_HOSEI;
+    float _jacket_image_ssize = JACKET_SIZE * S_HOSEI;
 	float _x_y_hosei = JACKET_SIZE-_jacket_image_ssize;
-    for (int i= 0; i<[dataArray count]; i++) {
-        NSLog(@"%d,%d",x,y);
-		// Made mask layer.
-		CALayer *roundRectLayer = [CALayer layer];
-		roundRectLayer.frame = CGRectMake(0, 0, _jacket_image_ssize, _jacket_image_ssize);
-		UIGraphicsBeginImageContextWithOptions(CGSizeMake(_jacket_image_ssize, _jacket_image_ssize), NO, 0.0); // iOS4
-		//UIGraphicsBeginImageContext(CGSizeMake(JACKET_SIZE, JACKET_SIZE)); // iOS3
-		UIBezierPath *roundRectPath = [UIBezierPath bezierPathWithRoundedRect:roundRectLayer.frame cornerRadius:10.f];
-		[[UIColor blackColor] setFill];
-		[roundRectPath fill];
-		UIImage *img = UIGraphicsGetImageFromCurrentImageContext();
-		UIGraphicsEndImageContext();
-		roundRectLayer.contents = (id)img.CGImage;
-		
-        UIImageView *aImageView = [[UIImageView alloc] initWithImage:[dataArray objectAtIndex:i]];
-        aImageView.frame = CGRectMake(JACKET_SIZE * x, JACKET_SIZE * y, _jacket_image_ssize, _jacket_image_ssize);
-		aImageView.layer.masksToBounds = YES;	//UIImageView's layer mask is YES.
-		aImageView.layer.mask = roundRectLayer;	//Insert mask layer.
-		
-        [self.scrollView addSubview:aImageView];
+    for (int i = 0; i < [dataArray count]; i++) {
+        NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+        
+        CALayer *roundRectLayer = [CALayer layer];
+        roundRectLayer.frame = CGRectMake(0, 0, _jacket_image_ssize, _jacket_image_ssize);
+        UIGraphicsBeginImageContextWithOptions(CGSizeMake(_jacket_image_ssize, _jacket_image_ssize), NO, 0.f);
+        UIBezierPath *roundRectPath = [UIBezierPath bezierPathWithRoundedRect:roundRectLayer.frame cornerRadius:10.f];
+        [[UIColor blackColor] setFill];
+        [roundRectPath fill];
+        UIImage *img = UIGraphicsGetImageFromCurrentImageContext();
+        UIGraphicsEndImageContext();
+        roundRectLayer.contents = (id)img.CGImage;
         UIButton *aButton = [UIButton buttonWithType:UIButtonTypeCustom];
-        //aButton.frame = aImageView.frame;
-		aButton.frame = CGRectMake(JACKET_SIZE * x, JACKET_SIZE * y, JACKET_SIZE, JACKET_SIZE);
+        aButton.frame = CGRectMake(JACKET_SIZE * x, JACKET_SIZE * y, _jacket_image_ssize, _jacket_image_ssize);
+        
+        [CATransaction unlock];
+        CALayer *albumLayer = [CALayer layer];
+        [albumLayer setFrame:CGRectMake(0, 0, aButton.frame.size.width, aButton.frame.size.height)];
+        [albumLayer setBackgroundColor:[UIColor clearColor].CGColor];
+        UIImage *aimg = [dataArray objectAtIndex:i];
+        [albumLayer setContents:(id)aimg.CGImage];
+        albumLayer.masksToBounds = YES;
+        albumLayer.mask = roundRectLayer;
+        [aButton.layer addSublayer:albumLayer];
+        aButton.layer.shadowColor = [UIColor blackColor].CGColor;
+        aButton.layer.shadowOpacity = 1.f;
+        aButton.layer.shadowOffset = CGSizeMake(0.f, 0.f);
+        aButton.layer.shadowRadius = 4.f;
+        [CATransaction flush];
+        [CATransaction lock];
+        
         aButton.tag = i;
         [aButton addTarget:self action:@selector(albumTapped:) forControlEvents:UIControlEventTouchUpInside];
         [self.scrollView addSubview:aButton];
@@ -111,9 +132,32 @@
         if (x == 4) {
             y += 1;
             x = 0;
-            self.scrollView.contentSize = CGSizeMake(320, JACKET_SIZE * y+1);
+            
         }
+        NSInteger idx = [_albumQueue indexOfObjectIdenticalTo:dataArray];
+        if (idx != NSNotFound) {
+            [_albumQueue removeObjectAtIndex:idx];
+        }
+        [pool drain];
     }
+    if (x == 4) {
+        self.scrollView.contentSize = CGSizeMake(320, JACKET_SIZE * y+1);
+    }
+    _albumThreadRunning = NO;
+    [CATransaction unlock];
+}
+
+#pragma mark -
+#pragma mark scrollViewContents
+-(void) setAlbumDataToScrollView {
+    if (!_albumQueue) {
+        _albumQueue = [[NSMutableArray alloc] init];
+    }
+    if ([_albumQueue indexOfObjectIdenticalTo:dataArray] == NSNotFound) {
+        [_albumQueue setArray:dataArray];
+    }
+    [self loadAlbumInBackground];
+    
 }
 
 -(void)albumTapped:(id)sender {
@@ -122,7 +166,7 @@
     
     UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"" message:[NSString stringWithFormat: @"%@",_dict] delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil];
     [alert show];
-
+    
 }
 
 #pragma mark -
